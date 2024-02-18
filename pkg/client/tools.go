a8cf3cda7569317a38c2ea9af6c5c5d192f34e45
samgozman <sam@gozman.space>
2024-02-18 10:00:57 +0400

    fix: go mod tidy issue

--- pkg/client/tools.go
+++ pkg/client/tools.go
@@ -3,7 +3,7 @@
 package client
 
 import (
-	_ "github.com/deepmap/oapi-codegen/v2"
+	_ "github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen"
 )
 
 // This file is used to declare the tools.go build tag. It is used to include the
