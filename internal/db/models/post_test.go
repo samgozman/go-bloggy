91836e285d6e1ea8126d531d5762eb4ab3651e1a
samgozman <sam@gozman.space>
2024-02-26 09:07:14 +0400

    Revert "chore: move count test"
    This reverts commit 9f62c594ea2543af49ef4cd65e6e93d2ddefcb24.

--- internal/db/models/post_test.go
+++ internal/db/models/post_test.go
@@ -186,28 +186,6 @@ func TestPostDB(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, "Updated Title", updatedPost.Title)
 	})
-
-	t.Run("Count", func(t *testing.T) {
-		t.Run("return count of all posts", func(t *testing.T) {
-			for i := 0; i < 5; i++ {
-				post := &Post{
-					UserID:      user.ID,
-					Slug:        uuid.New().String(),
-					Title:       "Test Title",
-					Description: "Test Description",
-					Content:     "Test Content",
-					Keywords:    "some",
-				}
-
-				err := postDB.Create(context.Background(), post)
-				assert.NoError(t, err)
-			}
-
-			count, err := postDB.Count(context.Background())
-			assert.NoError(t, err)
-			assert.Equal(t, int64(5), count)
-		})
-	})
 }
 
 func TestPost_Validate(t *testing.T) {
@@ -386,5 +364,42 @@ func TestPost_CountReadingTime(t *testing.T) {
 		}
 
 		assert.Zero(t, post.CountReadingTime().Seconds())
+	})
+}
+
+func TestPostDB_Count(t *testing.T) {
+	db, err := NewTestDB("file::memory:")
+	assert.NoError(t, err)
+
+	// insert a user to db
+	user := &User{
+		ExternalID: uuid.New().String(),
+		Login:      uuid.New().String(),
+		AuthMethod: GitHubAuthMethod,
+	}
+
+	err = db.WithContext(context.Background()).Create(user).Error
+	assert.NoError(t, err)
+
+	postDB := NewPostDB(db)
+
+	t.Run("return count of all posts", func(t *testing.T) {
+		for i := 0; i < 5; i++ {
+			post := &Post{
+				UserID:      user.ID,
+				Slug:        uuid.New().String(),
+				Title:       "Test Title",
+				Description: "Test Description",
+				Content:     "Test Content",
+				Keywords:    "some",
+			}
+
+			err := postDB.Create(context.Background(), post)
+			assert.NoError(t, err)
+		}
+
+		count, err := postDB.Count(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, int64(5), count)
 	})
 }
