e518b3fa304bec2c0d224ec42f3e688877068390
samgozman <sam@gozman.space>
2024-04-06 09:25:52 +0400

    chore

--- internal/db/provider.go
+++ internal/db/provider.go
@@ -1,10 +1,8 @@
 package db
 
 import (
-	"fmt"
 	"github.com/google/wire"
 	"github.com/samgozman/go-bloggy/internal/config"
-	"github.com/samgozman/go-bloggy/internal/db/models"
 )
 
 // ProvideDSN provides the DSN from the config.
@@ -14,25 +12,7 @@ func ProvideDSN(cfg *config.Config) string {
 
 // ProvideDatabase provides a new database connection.
 func ProvideDatabase(dsn string) (*Database, error) {
-	db, err := connectToPG(dsn)
-	if err != nil {
-		return nil, fmt.Errorf("%w: %w", ErrFailedToConnectDatabase, err)
-	}
-
-	// Migrate the schema
-	err = db.AutoMigrate(&models.User{}, &models.Post{}, &models.Subscriber{})
-	if err != nil {
-		return nil, fmt.Errorf("%w: %w", ErrFailedToMigrateDatabase, err)
-	}
-
-	return &Database{
-		Conn: db,
-		Models: &Models{
-			Users:       models.NewUserDB(db),
-			Posts:       models.NewPostDB(db),
-			Subscribers: models.NewSubscribersDB(db),
-		},
-	}, nil
+	return InitDatabase(dsn)
 }
 
 // ProviderSet is a wire provider set that provides the database connection.
