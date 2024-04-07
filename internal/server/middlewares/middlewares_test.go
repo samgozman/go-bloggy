7ae0e11ca600415a3fa24e6cbdbb5a878dc7baf4
samgozman <sam@gozman.space>
2024-04-07 18:05:41 +0400

    test: add more tests for middleware

--- internal/server/middlewares/middlewares_test.go
+++ internal/server/middlewares/middlewares_test.go
@@ -60,4 +60,35 @@ func Test_JWTAuth(t *testing.T) {
 		assert.Equal(t, http.StatusUnauthorized, rec.Code)
 		assert.Equal(t, fmt.Sprintf("\"%s\"\n", ErrAuthHeaderRequired), rec.Body.String())
 	})
+
+	t.Run("skip allowed paths", func(t *testing.T) {
+		t.Run("GET request", func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			rec := httptest.NewRecorder()
+			ctx := echo.New().NewContext(req, rec)
+
+			_ = middleware(func(c echo.Context) error {
+				return c.String(http.StatusOK, "test")
+			})(ctx)
+
+			assert.Equal(t, http.StatusOK, rec.Code)
+			assert.Equal(t, "test", rec.Body.String())
+		})
+
+		testCases := []string{"/login", "/subscribers"}
+		for _, path := range testCases {
+			t.Run(path, func(t *testing.T) {
+				req := httptest.NewRequest(http.MethodPost, path, nil)
+				rec := httptest.NewRecorder()
+				ctx := echo.New().NewContext(req, rec)
+
+				_ = middleware(func(c echo.Context) error {
+					return c.String(http.StatusOK, "test")
+				})(ctx)
+
+				assert.Equal(t, http.StatusOK, rec.Code)
+				assert.Equal(t, "test", rec.Body.String())
+			})
+		}
+	})
 }
