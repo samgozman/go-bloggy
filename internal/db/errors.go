8bcb4da68de81e27cf35947205915d877c627f1c
samgozman <sam@gozman.space>
2024-04-07 15:22:12 +0400

    chore

--- internal/db/errors.go
+++ internal/db/errors.go
@@ -3,8 +3,6 @@ package db
 import "errors"
 
 var (
-	ErrFailedToConnectDatabase             = errors.New("ERR_FAILED_TO_CONNECT_DATABASE")
-	ErrFailedToMigrateDatabase             = errors.New("ERR_FAILED_TO_MIGRATE_DATABASE")
-	ErrFailedToGetDatabaseConnection       = errors.New("ERR_FAILED_TO_GET_DATABASE_CONNECTION")
-	ErrFailedToEnableForeignKeyConstraints = errors.New("ERR_FAILED_TO_ENABLE_FOREIGN_KEY_CONSTRAINTS")
+	ErrFailedToConnectDatabase = errors.New("ERR_FAILED_TO_CONNECT_DATABASE")
+	ErrFailedToMigrateDatabase = errors.New("ERR_FAILED_TO_MIGRATE_DATABASE")
 )
